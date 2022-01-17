887b0c9b32b6ba42eaf1cc596b9e6c3af8d55b45
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-01-17 20:19:47 -0300

    Inverti a lógica do "if" para remover o "pass"

--- Lista de exercícios/Lista 005/q003.py
+++ Lista de exercícios/Lista 005/q003.py
@@ -8,9 +8,7 @@ for c in range(10):
   vetor.append(randint(0, 100))
 print(vetor)
 for c in range(10):
-  if vetor[c] % 2 == 0 and vetor[c] % 1 == 0:
-      pass
-  else:
+  if vetor[c] % 2 != 0 and vetor[c] % 1 != 0:
     con_primos += 1
     lista_primos.append(vetor[c])
 print(f"A quantidade de números primos é {con_primos}. {lista_primos}")
