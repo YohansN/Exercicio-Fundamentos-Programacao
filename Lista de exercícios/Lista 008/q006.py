25cd0ffadab8e608cae4188853f8d432fc2c74db
Yohans Nascimento <yohans.dev@gmail.com>
2022-02-14 16:05:37 -0300

    Questão 6 - Refatorada

--- Lista de exercícios/Lista 008/q006.py
+++ Lista de exercícios/Lista 008/q006.py
@@ -5,45 +5,30 @@ No final, o programa deverá emitir as seguintes informações:
 a) Nome dos alunos com suas médias;
 b) A maior e a menor média;'''
 
-aluno1 = []
-aluno2 = []
-aluno3 = []
-ordenador_media =[]
-
-for i in range(0, 3):       #LINHA
-    for j in range(0,3):    #COLUNA
+boletim = [[0,0,0],[0,0,0],[0,0,0]]
+boletim_medias = [[0,0],[0,0],[0,0]]
+medias = []
+for i in range(0, 3):
+    for j in range(0,3):
         if j == 0:
-            nome = str(input("Nome do aluno(a): "))
-            if i == 0:
-                aluno1.append(nome)
-            elif i == 1:
-                aluno2.append(nome)
-            else:
-                aluno3.append(nome)
-        elif j == 1:
-            n1 = float(input("Primeira Nota: "))
-            if i == 0:
-                aluno1.append(n1)
-            elif i == 1:
-                aluno2.append(n1)
-            else:
-                aluno3.append(n1)
+            boletim[i][j] = str(input("Nome do aluno(a): "))
+            boletim_medias[i][0] = boletim[i][j]
         else:
-            n2 = float(input("Segunda Nota: "))
-            if i == 0:
-                aluno1.append(n2)
-            elif i == 1:
-                aluno2.append(n2)
-            else:
-                aluno3.append(n2)
+            boletim[i][j] = float(input(f"{j}° Nota")) 
 
-print("-="*25)
-media1 = (aluno1[1] + aluno1[2]) / 2
-ordenador_media.append(media1)
-media2 = (aluno2[1] + aluno2[2]) / 2
-ordenador_media.append(media2)
-media3 = (aluno3[1] + aluno3[2]) / 2
-ordenador_media.append(media3)
-ordenador_media_crescente = sorted(ordenador_media)
-print(f" Aluno(a): {aluno1[0]} - Média: {media1} \n Aluno(a): {aluno2[0]} - Média: {media2} \n Aluno(a): {aluno3[0]} - Média: {media3} \n")
-print(f"Maior média: {ordenador_media_crescente[2]} - Menor média: {ordenador_media_crescente[0]}")
+for i in range(0, 3):
+    for j in range(1,3): #Percorre apenas as notas, somando-as.    
+        boletim_medias[i][1] += boletim[i][j]
+#print(boletim_medias)
+
+for i in range(0, 3):
+    for j in range(1,2):
+        boletim_medias[i][1] = boletim_medias[i][1] / 2 #Faz a média das notas.
+#print(boletim_medias)
+
+for i in range(0,3):
+    medias.append(boletim_medias[i][1])
+    medias = sorted(medias)
+
+print(f" Aluno(a): {boletim_medias[0][0]} - Média: {boletim_medias[0][1]:.2f} \n Aluno(a): {boletim_medias[1][0]} - Média: {boletim_medias[1][1]:.2f} \n Aluno(a): {boletim_medias[2][0]} - Média: {boletim_medias[2][1]:.2f} \n")
+print(f"Maior média: {medias[2]:.2f} \n Menor média: {medias[0]:.2f}.")
